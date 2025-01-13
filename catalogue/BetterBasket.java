2409c183f31cdb67079ac63d58e44490ed57cbbc
rubenjohnson123 <R.Johnson17@uni.brighton.ac.uk>
2025-01-13 16:41:45 +0000

    Test V1

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -13,11 +13,6 @@ public class BetterBasket extends Basket implements Serializable
 {
   private static final long serialVersionUID = 1L;
 
-  private static void main(String args[]) {
-    System.out.println("Hello World");
-  }
-  
-
   // You need to add code here
   // merge the items for same product,
   // or sort the item based on the product number
